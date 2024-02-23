a76401578c0f54f30d925be85dfa6d490e3f7c9e
tetianaklius <tetyanaklyus@gmail.com>
2024-02-23 20:42:45 +0200

    hw 1

--- lesson_1/hw_1.js
+++ lesson_1/hw_1.js
@@ -1,19 +1,19 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Вивести кожну змінну за допомогою: console.log
 
-a = "hello";
-b = "owu";
-c = "com";
-d = "ua";
-e = 1;
-f = 10;
-g = -999;
-h = 123;
-i = 3.14;
-j = 2.7;
-k = 16;
-l = true;
-m = false;
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
 console.log(a)
 console.log(b)
 console.log(c)
@@ -30,10 +30,10 @@ console.log(m)
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
-firstName = "Tetiana";
-middleName = "Oleksandrivna";
-lastName = "Klius";
-person = firstName + " " + middleName + " " + lastName;
+let firstName = "Tetiana";
+let middleName = "Oleksandrivna";
+let lastName = "Klius";
+let person = firstName + " " + middleName + " " + lastName;
 console.log(person);
 
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
